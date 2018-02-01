89dc3cb64f61305ad066952786f1dcda248b5986
Aaron En Ye Shi <enye.shi@gmail.com>
2018-02-01 20:09:46 +0000

    Fixes MXNet HIP kernel launch issues and variable argument instantiations

--- src/operator/tensor/indexing_op-inl.cuh
+++ src/operator/tensor/indexing_op-inl.cuh
@@ -244,26 +244,26 @@ inline void AddTakeGradLargeBatch(mshadow::Tensor<gpu, 2, DType> dst,
 
   switch (SZ) {
     case 1:
-    hipLaunchKernelGGL(HIP_KERNEL_NAME(AddTakeGradLargeBatchKernel<1, DType>), dim3(dimGrid), dim3(dimBlock), shmem_size, stream, dst.dptr_, sum_counts_ptr, num_runs_ptr,
-         sorted.dptr_, index.dptr_, src.dptr_,
+    hipLaunchKernelGGL(HIP_KERNEL_NAME(AddTakeGradLargeBatchKernel<1, DType>), dim3(dimGrid), dim3(dimBlock), shmem_size, stream, dst.dptr_, static_cast<const IndexType*>(sum_counts_ptr), static_cast<const int*>(num_runs_ptr),
+         static_cast<const IndexType*>(sorted.dptr_), static_cast<const IndexType*>(index.dptr_), static_cast<const DType*>(src.dptr_),
          static_cast<int>(src.size(0)),
          static_cast<int>(src.size(1)));
     break;
     case 2:
-    hipLaunchKernelGGL(HIP_KERNEL_NAME(AddTakeGradLargeBatchKernel<2, DType>), dim3(dimGrid), dim3(dimBlock), shmem_size, stream, dst.dptr_, sum_counts_ptr, num_runs_ptr,
-         sorted.dptr_, index.dptr_, src.dptr_,
+    hipLaunchKernelGGL(HIP_KERNEL_NAME(AddTakeGradLargeBatchKernel<2, DType>), dim3(dimGrid), dim3(dimBlock), shmem_size, stream, dst.dptr_, static_cast<const IndexType*>(sum_counts_ptr), static_cast<const int*>(num_runs_ptr),
+         static_cast<const IndexType*>(sorted.dptr_), static_cast<const IndexType*>(index.dptr_), static_cast<const DType*>(src.dptr_),
          static_cast<int>(src.size(0)),
          static_cast<int>(src.size(1)));
     break;
     case 3:
-    hipLaunchKernelGGL(HIP_KERNEL_NAME(AddTakeGradLargeBatchKernel<3, DType>), dim3(dimGrid), dim3(dimBlock), shmem_size, stream, dst.dptr_, sum_counts_ptr, num_runs_ptr,
-         sorted.dptr_, index.dptr_, src.dptr_,
+    hipLaunchKernelGGL(HIP_KERNEL_NAME(AddTakeGradLargeBatchKernel<3, DType>), dim3(dimGrid), dim3(dimBlock), shmem_size, stream, dst.dptr_, static_cast<const IndexType*>(sum_counts_ptr), static_cast<const int*>(num_runs_ptr),
+         static_cast<const IndexType*>(sorted.dptr_), static_cast<const IndexType*>(index.dptr_), static_cast<const DType*>(src.dptr_),
          static_cast<int>(src.size(0)),
          static_cast<int>(src.size(1)));
     break;
     case 4:
-    hipLaunchKernelGGL(HIP_KERNEL_NAME(AddTakeGradLargeBatchKernel<4, DType>), dim3(dimGrid), dim3(dimBlock), shmem_size, stream, dst.dptr_, sum_counts_ptr, num_runs_ptr,
-         sorted.dptr_, index.dptr_, src.dptr_,
+    hipLaunchKernelGGL(HIP_KERNEL_NAME(AddTakeGradLargeBatchKernel<4, DType>), dim3(dimGrid), dim3(dimBlock), shmem_size, stream, dst.dptr_, static_cast<const IndexType*>(sum_counts_ptr), static_cast<const int*>(num_runs_ptr),
+         static_cast<const IndexType*>(sorted.dptr_), static_cast<const IndexType*>(index.dptr_), static_cast<const DType*>(src.dptr_),
          static_cast<int>(src.size(0)),
          static_cast<int>(src.size(1)));
     break;
